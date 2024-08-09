524d25d5cae8de938432a923d6106ce71b6be26e
KudoC0nan <patilshreyasvjti@gmail.com>
2024-08-09 20:49:18 +0530

    added sidebar and chart components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import Siderbar from "../../components/Siderbar";
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Shreyas', lastName: 'Patil' }
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Siderbar user={loggedIn} />
         {children}
     </main>
   );
